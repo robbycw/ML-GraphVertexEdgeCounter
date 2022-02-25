d5188cfde598f0bf25c405e595f24228d95a1aca
robbycw <robbycw9@gmail.com>
2022-02-25 16:27:45 -0500

    Basic Graph Data Generation
    Created Sage Program that can generate and save images of graphs with a given range of vertices.
    Iterates through graphs with a number of edges equal to the number of vertices up to one less than the maximum number of edges.
    Added folder with example graph data generated by this program.

--- sageDataMaker.py
+++ sageDataMaker.py
@@ -1,1 +1,8 @@
 # This program can create and save the images of graphs used in the dataset. 
+
+path = 'C:/Users/bsawe/Documents/GitHub/ML-GraphVertexEdgeCounter/graphdata/'
+for i in range(3,10): # number of vertices 
+    for j in range (i, i*(i-1)/2): # number of edges
+        g = graphs.RandomGNM(i, j)
+        name = path + str(i) + 'x' + str(j) + '.png'
+        g.plot().save(name)
