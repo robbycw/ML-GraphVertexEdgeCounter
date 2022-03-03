ff72286288ff39f5a11103ecd44396a723006caf
robbycw <robbycw9@gmail.com>
2022-03-03 13:00:34 -0500

    Added Data Transforms
    Rescale, RandomCrop, ToTensor added to transforms.py
    
    Print tests  in dataset.pt show that the images are properly transformed.
    
    DataLoader is able to iterate over my custom Dataset class.

--- dataset.py
+++ dataset.py
@@ -15,6 +15,8 @@ import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 
+from transforms import Rescale, RandomCrop, ToTensor
+
 # Ignore warnings
 import warnings
 warnings.filterwarnings("ignore")
@@ -59,3 +61,25 @@ vert_dataset = VertexGraphImageDatasets(csv_file='C:/Users/bsawe/Documents/GitHu
 for i in range(len(vert_dataset)):
     sample = vert_dataset[i]
     print(i, sample['image'].shape, sample['vertices'])
+
+# This following test does the same thing, but applies transforms to the images and makes sure that the image sizes were changed. 
+transformed_dataset = VertexGraphImageDatasets(csv_file='C:/Users/bsawe/Documents/GitHub/ML-GraphVertexEdgeCounter/graphdata/verticesCount.csv', 
+                                        root_dir='C:/Users/bsawe/Documents/GitHub/ML-GraphVertexEdgeCounter/graphdata/',
+                                        transform=transforms.Compose([Rescale(256), RandomCrop(224), ToTensor()]))
+
+for i in range(len(transformed_dataset)):
+    sample = transformed_dataset[i]
+    print(i, sample['image'].size(), sample['vertices'])
+
+    if i == 5: # We only need to see the first few to confirm the transforms are working. 
+        break
+
+# This tests applies transforms and iterates over the dataset with a DataLoader. 
+
+dataloader = DataLoader(transformed_dataset, batch_size=4, shuffle=True, num_workers=0)
+
+for i_batch, sample_batched in enumerate(dataloader):
+    print(i_batch, sample_batched['image'].size(), sample_batched['vertices'])
+
+    if i_batch == 3: 
+        break
